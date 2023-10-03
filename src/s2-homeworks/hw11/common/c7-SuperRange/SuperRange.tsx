5215f3ecc05e2cee5443eb8fe2a3a7115f597edc
Eugen <vasilenkoevgen88@gmail.com>
2023-10-03 13:02:28 +0300

    Slider HomeWork

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
+
 const SuperRange: React.FC<SliderProps> = (props) => {
+
     return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+        <Slider sx={{ // стили для слайдера // пишет студент
+            width: '100%',
+            maxWidth: "150px",
+            heightMax: "4px",
+            borderRadius: '10px',
+            color: '#0C2',
+            '& .MuiSlider-rail': {
+                backgroundColor: 'rgba(139, 139, 139, 1)', // Замените 'your-color-here' на цвет фона
+            },
+        }}
+                {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
 }
