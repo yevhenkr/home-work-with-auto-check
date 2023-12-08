a98990087d71be08ccff3222b91cb8a7bb620dc7
Eugen <vasilenkoevgen88@gmail.com>
2023-12-08 09:26:58 +0200

    css

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -12,9 +12,9 @@ import HW13 from "../../hw13/HW13";
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
-            <HW10/>
-            <HW11/>
-            <HW12/>
+            {/*<HW10/>*/}
+            {/*<HW11/>*/}
+            {/*<HW12/>*/}
             <HW13 />
             {/*<HW14 />*/}
             {/*<HW15 />*/}
